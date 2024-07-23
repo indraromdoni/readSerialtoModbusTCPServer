661f55a0a5990a76ef9ab0db87e424fe7a24195b
indraromdoni <indraromdoni65@gmail.com>
2024-07-23 10:31:39 +0700

    readSerial.py add reply code writeSerial.py add read serial code

--- readSerial.py
+++ readSerial.py
@@ -3,7 +3,19 @@ import time
 
 ser = serial.Serial("COM1", 9600)
 while True:
-    print("read", ser.read(1024).decode('utf-8'))
+    rd = ser.read(1024).decode('utf-8').rstrip()
+    print("read", rd)
+    if rd == ":T1000o":
+        #return
+        print("Test")
+        ser.write("Testing code".encode("utf-8"))
+    elif rd == ":R2100k":
+        #return
+        print("Read")
+        ser.write("Read code".encode("utf-8"))
+    elif rd == ":R2400n":
+        #return
+        print("Recall")
+        ser.write("Recall code".encode("utf-8"))
+
     time.sleep(1)
-    if KeyboardInterrupt:
-        break
