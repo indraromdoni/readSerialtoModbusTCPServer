981380eb92b9d0ad3fa01badff768ae6574826b2
indraromdoni <indraromdoni65@gmail.com>
2024-07-26 16:31:44 +0700

    update code

--- modbusServer.py
+++ modbusServer.py
@@ -1,4 +1,4 @@
-from pyModbusTCP.server import ModbusServer, DataBank, DataHandler
+from pyModbusTCP.server import ModbusServer, DataBank
 from time import sleep
 import random
 
@@ -10,7 +10,8 @@ try:
     while True:
         r = random.randint(0, 100)
         print(r)
-        ret = DataBank.set_holding_registers(self=server.data_bank, address=0, word_list=[r,1,2,3])
+        #ret = DataBank.set_holding_registers(self=server.data_bank, address=0, word_list=[r,1,2,3])
+        ret = DataBank.set_words(address=0, word_list=[r,1,2,3])
         sleep(1)
 except Exception as e:
     print("Shutdown server:", e)
